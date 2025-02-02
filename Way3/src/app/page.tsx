cb5d1ebceaa0975e560ff914f81b8166a4fd71a7
Nabeel Rahman <mailnabeelrahman227@gmail.com>
2025-02-02 22:46:16 +0530

    add header for users data in Home component

--- Way3/src/app/page.tsx
+++ Way3/src/app/page.tsx
@@ -13,6 +13,7 @@ export default async function Home() {
   const userList = await DB.select().from(users).all();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Users Data</h1>
         <UserList initialUsers={userList} />
     </main>
   );
